64455e0f91d1830b29431d080bcde7a616f59ee2
Emil Trebicka <emil.trebicka@thoughtworks.com>
2023-10-17 11:48:25 +0200

    removed empty check on evaluate score as it is never reached

--- src/routes/fen/evaluate_score.rs
+++ src/routes/fen/evaluate_score.rs
@@ -5,16 +5,6 @@ pub async fn evaluate_score(info: web::Path<String>) -> HttpResponse {
     let fen_string = info.into_inner();
     let evaluation_result = EvaluationResult::new(&fen_string, -11.0);
 
-    if evaluation_result.is_err() {
-        // return bad request with error message
-        return HttpResponse::BadRequest()
-            .content_type("application/json")
-            .body(format!(
-                "Failed to evaluate score: {:?}",
-                evaluation_result.err()
-            ));
-    }
-
     let serialized_result = serde_json::to_string(&evaluation_result.unwrap())
         .expect("Failed to serialize evaluation result to JSON");
 
