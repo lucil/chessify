14eef2adc4e37f728ec71ff8718a81a3267d881b
Emil Trebicka <emil.trebicka@thoughtworks.com>
2023-10-17 11:23:59 +0200

    changed tarpaulin in pipeline

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use chessify::startup::run;
 use std::net::TcpListener;
 
+#[cfg(not(tarpaulin_include))]
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let address = TcpListener::bind("127.0.0.1:8000")?;
