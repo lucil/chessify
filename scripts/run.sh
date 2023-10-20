76481545c655a0ae0083817290903f8fa04b5d7e
Emil Trebicka <emil.trebicka@thoughtworks.com>
2023-10-20 12:54:51 +0200

    fixed clippy issues

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,4 +3,4 @@ cargo build
 cargo test
 cargo clippy --verbose --all-targets --all-features -- -D warnings
 cargo fmt --verbose --all -- --check
-cargo-tarpaulin --manifest-path ../Cargo.toml 
+cargo-tarpaulin --manifest-path ../Cargo.toml --out Html --output-dir ../coverage
